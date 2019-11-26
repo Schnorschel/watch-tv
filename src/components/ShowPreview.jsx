2dde5c3878a65011cc408b02472d02f37595296b
Georg Oehl <oehl@pobox.com>
2019-11-26 17:55:58 -0500

    Added logic for hook

--- src/components/ShowPreview.jsx
+++ src/components/ShowPreview.jsx
@@ -1,5 +1,28 @@
-import React from 'react'
+import React, { setState, useEffect } from 'react'
+import axios from 'axios'
 
-return <section className="topratedShowsContainer"></section>
+const ShowPreview = () => {
+  const [showData, setShowData] = setState([])
 
-export default FeaturedShow
+  const getShowInfo = async () => {
+    const resp = await axios.get(
+      'https://api.themoviedb.org/3/tv/top_rated?api_key=5a39bf29dd3b617bf0c511dbf50b9b2d&language=en-US&page=1'
+    )
+  }
+
+  useEffect(() => {
+    getShowInfo()
+  }, [showData])
+
+  return (
+    <section className="previewShowsContainer">
+      <section className="previewShowContainer">
+        <section className="previewShowImageContainer">
+          <img src="" />
+        </section>
+        <p className="previewShowTitle"></p>
+      </section>
+    </section>
+  )
+}
+export default ShowPreview
