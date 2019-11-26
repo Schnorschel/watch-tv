ce58e2f252ad549f212e7520bc6b3648adb7834d
Georg Oehl <oehl@pobox.com>
2019-11-26 15:48:56 -0500

    Renamed/reorganized pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
-import Page from './pages/Page'
-import Page2 from './pages/Page2'
+import TVShowPage from './pages/TVShowPage'
 import HomePage from './pages/HomePage'
 import NotFound from './pages/NotFound'
 
@@ -9,25 +8,18 @@ const App = () => {
   return (
     <Router>
       <header>
-        <h1>Welcome to my SPA</h1>
+        <h1>Top-rated TV Shows</h1>
         <nav>
           <ul>
             <li>
               <Link to="/">Go Home</Link>
-            </li>
-            <li>
-              <Link to="/1">Page 1</Link>
-            </li>
-            <li>
-              <Link to="/2">Page 2</Link>
             </li>
           </ul>
         </nav>
       </header>
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
-        <Route exact path="/1" component={Page}></Route>
-        <Route exact path="/2" component={Page2}></Route>
+        <Route exact path="/TVShowPage/:showId" component={TVShowPage}></Route>
         <Route path="*" component={NotFound}></Route>
       </Switch>
     </Router>
---
+++ src/pages/TVShowPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Page = () => {
+  return <div>This is the TV Show Page</div>
+}
+
+export default TVShowPage
