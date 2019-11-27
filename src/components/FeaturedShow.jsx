e75332b0a944b4bf063d490d08146cfc5b259ea3
Georg Oehl <oehl@pobox.com>
2019-11-27 15:06:16 -0500

    Beginning of implementation of Featured Show

--- src/components/FeaturedShow.jsx
+++ src/components/FeaturedShow.jsx
@@ -5,29 +5,32 @@ const FeaturedShow = props => {
   const [showData, setShowData] = useState([])
   const [featuredShow, setFeaturedShow] = useState()
 
+  // prettier-ignore
   const getFeaturedShowInfo = async () => {
-    const resp = await axios.get(
-      'https://api.themoviedb.org/3/tv/' +
-        props.showId +
-        '?api_key=5a39bf29dd3b617bf0c511dbf50b9b2d&language=en-US&append_to_response=credits'
-    )
-    setShowData(resp.data.results)
-    // setFeaturedShow()
+    console.log('Attempting to get: ' + 'https://api.themoviedb.org/3/tv/' + props.showId + '?api_key=5a39bf29dd3b617bf0c511dbf50b9b2d&language=en-US&append_to_response=credits')
+    if (typeof props.showId === 'undefined') {
+      return
+    }
+    const resp = await axios.get('https://api.themoviedb.org/3/tv/' + props.showId + '?api_key=5a39bf29dd3b617bf0c511dbf50b9b2d&language=en-US&append_to_response=credits')
+    setShowData(resp.data)
+    console.log(resp.data)
   }
 
   useEffect(() => {
     getFeaturedShowInfo()
-  }, [])
+  }, [props.showId])
 
+  // prettier-ignore
   return (
-    <section className="featuredShowContainer">
-      <h1></h1>
-      {/* <section className="featuredShowImageContainer"> */}
-      <img className="featuredShowImage" />
-      <p></p>
-    </section>
-    // </section>
+       showData ? 
+        <>
+        <h1>{showData.name}</h1>
+          <img className="featuredShowImage" />
+          <p></p> 
+        </>
+        : 'Loading featured TV Show data...' 
   )
+  // </section>
 }
 
 export default FeaturedShow
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,28 +4,35 @@ import FeaturedShow from '../components/FeaturedShow'
 import ShowPreview from '../components/ShowPreview'
 import Footer from '../components/Footer'
 
+// prettier-ignore
 const HomePage = () => {
   const imageBranch = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2'
   const [showData, setShowData] = useState([])
-  const [featuredShowId, setFeaturedShowId] = useState(
-    Math.floor(Math.random() * 20)
-  )
+  const [featuredShowIndex, setFeaturedShowIndex] = useState( Math.floor(Math.random() * 20) )
+  const [featuredShowId, setFeaturedShowId] = useState()
 
   // prettier-ignore
   const getShowInfo = async () => {
   const resp = await axios.get('https://api.themoviedb.org/3/tv/top_rated?api_key=5a39bf29dd3b617bf0c511dbf50b9b2d&language=en-US&page=1')
   setShowData(resp.data.results)
   console.log(resp.data.results)
+  setFeaturedShowId(resp.data.results[featuredShowIndex].id)
   }
 
   useEffect(() => {
     getShowInfo()
   }, [])
 
+  // const featuredShowId = showData.filter((show, index) => {
+  //   return index === featuredShowIndex
+  // })[0].id
+
   // prettier-ignore
   return (
     <>
-      <FeaturedShow showId={featuredShowId} />
+      <section className="featuredShowContainer">
+        {featuredShowId && <FeaturedShow showId={featuredShowId} /> }
+      </section>
       <section className="previewAllShowsContainer">
         {showData.map(show => {
           return (
