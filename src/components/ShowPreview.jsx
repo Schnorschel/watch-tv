1e69a9f9aaac133a23b2bbf3970bc8cc84e15815
Georg Oehl <oehl@pobox.com>
2019-11-26 19:04:31 -0500

    Bug fix and beginning of implementing map-function

--- src/components/ShowPreview.jsx
+++ src/components/ShowPreview.jsx
@@ -1,28 +1,29 @@
-import React, { setState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const ShowPreview = () => {
-  const [showData, setShowData] = setState([])
+  const [showData, setShowData] = useState([])
 
+  // prettier-ignore
   const getShowInfo = async () => {
-    const resp = await axios.get(
-      'https://api.themoviedb.org/3/tv/top_rated?api_key=5a39bf29dd3b617bf0c511dbf50b9b2d&language=en-US&page=1'
-    )
-    setShowData(resp.data.results)
+  const resp = await axios.get('https://api.themoviedb.org/3/tv/top_rated?api_key=5a39bf29dd3b617bf0c511dbf50b9b2d&language=en-US&page=1')
+  setShowData(resp.data.results)
   }
 
   useEffect(() => {
     getShowInfo()
-  }, [showData])
+  }, [])
 
   return (
     <section className="previewShowsContainer">
-      <section className="previewShowContainer">
-        <section className="previewShowImageContainer">
-          <img src="" />
+      {showData.map(
+        <section className="previewShowContainer">
+          <section className="previewShowImageContainer">
+            <img src="" />
+          </section>
+          <p className="previewShowTitle"></p>
         </section>
-        <p className="previewShowTitle"></p>
-      </section>
+      )}
     </section>
   )
 }
