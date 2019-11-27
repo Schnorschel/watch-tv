b623e2bff6556fcb89e3eae0f394d891a165ffc8
Georg Oehl <oehl@pobox.com>
2019-11-27 12:25:28 -0500

    Added API Call

--- src/components/FeaturedShow.jsx
+++ src/components/FeaturedShow.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const FeaturedShow = props => {
+  const [showData, setShowData] = useState([])
+  const [featuredShow, setFeaturedShow] = useState()
+
+  const getFeaturedShowInfo = async () => {
+    const resp = await axios.get(
+      'https://api.themoviedb.org/3/tv/' +
+        props.showId +
+        '?api_key=5a39bf29dd3b617bf0c511dbf50b9b2d&language=en-US&append_to_response=credits'
+    )
+    setShowData(resp.data.results)
+    // setFeaturedShow()
+  }
+
+  useEffect(() => {
+    getFeaturedShowInfo()
+  }, [])
+
   return (
     <section className="featuredShowContainer">
-      <section className="featuredShowImageContainer">
-        <img className="featuredShowImage" />
-      </section>
+      <h1></h1>
+      {/* <section className="featuredShowImageContainer"> */}
+      <img className="featuredShowImage" />
+      <p></p>
     </section>
+    // </section>
   )
 }
 
