a1fb00d73d95e60d29545abe8d5247c2134f7964
Georg Oehl <oehl@pobox.com>
2019-11-26 23:06:04 -0500

    Moved API logic to HomePage

--- src/components/ShowPreview.jsx
+++ src/components/ShowPreview.jsx
@@ -1,29 +1,12 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
 
-const ShowPreview = () => {
-  const [showData, setShowData] = useState([])
-
-  // prettier-ignore
-  const getShowInfo = async () => {
-  const resp = await axios.get('https://api.themoviedb.org/3/tv/top_rated?api_key=5a39bf29dd3b617bf0c511dbf50b9b2d&language=en-US&page=1')
-  setShowData(resp.data.results)
-  }
-
-  useEffect(() => {
-    getShowInfo()
-  }, [])
-
+const ShowPreview = props => {
   return (
-    <section className="previewShowsContainer">
-      {showData.map(
-        <section className="previewShowContainer">
-          <section className="previewShowImageContainer">
-            <img src="" />
-          </section>
-          <p className="previewShowTitle"></p>
-        </section>
-      )}
+    <section className="previewShowContainer">
+      <section className="previewShowImageContainer">
+        <img src={props.imagePath} alt="" />
+      </section>
+      <p className="previewShowTitle">{props.name}</p>
     </section>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import FeaturedShow from '../components/FeaturedShow'
 import ShowPreview from '../components/ShowPreview'
 import Footer from '../components/Footer'
 
 const HomePage = () => {
+  const imageBranch = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2'
+  const [showData, setShowData] = useState([])
+
+  // prettier-ignore
+  const getShowInfo = async () => {
+  const resp = await axios.get('https://api.themoviedb.org/3/tv/top_rated?api_key=5a39bf29dd3b617bf0c511dbf50b9b2d&language=en-US&page=1')
+  setShowData(resp.data.results)
+  console.log(resp.data.results)
+  }
+
+  useEffect(() => {
+    getShowInfo()
+  }, [])
+
+  // prettier-ignore
   return (
     <>
       {/* <FeaturedShow /> */}
-      <ShowPreview />
-      <Footer />
+      <section className="previewShowsContainer">
+        {showData.map(show => {
+          return (
+            <ShowPreview 
+              key={show.id}
+              imagePath={imageBranch + show.poster_path}
+              name={show.name}
+            />
+          )
+        })}
+      </section>
+      {/* <Footer /> */}
     </>
   )
 }
