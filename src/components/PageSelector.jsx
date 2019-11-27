5b64685cf923474e754e6ba41495ec2602a8312d
Georg Oehl <oehl@pobox.com>
2019-11-27 18:16:37 -0500

    Typo fix

--- src/components/PageSelector.jsx
+++ src/components/PageSelector.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
 const PageSelector = () => {
-  return
-  ;<section className="pageSelectorContainer"></section>
+  return <section className="pageSelectorContainer"></section>
 }
 
 export default PageSelector
