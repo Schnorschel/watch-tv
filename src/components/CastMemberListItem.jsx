8223b7294816cddf0b02baa4445ee27be6578c90
Georg Oehl <oehl@pobox.com>
2019-11-30 23:37:46 -0500

    Swapped order of character and actor name Added vertical padding between actor tiles

--- src/components/CastMemberListItem.jsx
+++ src/components/CastMemberListItem.jsx
@@ -5,17 +5,19 @@ const CastMemberListItem = prop => {
   const imageBranch185 = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2'
   return (
     <li className="castMemberLI">
-      <img
-        className="castMemberImage"
-        src={imageBranch185 + prop.actor.profile_path}
-        alt={'Picture of cast member ' + prop.actor.name}
-      />
+      <Link className="castMemberName" to={'/actor/' + prop.actor.id}>
+        <img
+          className="castMemberImage"
+          src={imageBranch185 + prop.actor.profile_path}
+          alt={'Picture of cast member ' + prop.actor.name}
+        />
+      </Link>
+      <p className="character">{prop.actor.character}</p>
       <p className="castMemberName">
         <Link className="castMemberName" to={'/actor/' + prop.actor.id}>
           {prop.actor.name}
         </Link>
       </p>
-      <p className="character">{prop.actor.character}</p>
     </li>
   )
 }
